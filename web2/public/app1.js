aed2de87f9645fd5c4a3476963f3daf4e55a764d
Deepak Kumar <91089955+xdpak07@users.noreply.github.com>
2022-04-03 12:14:50 +0530

    Add files via upload

---
+++ web2/public/app1.js
@@ -0,0 +1,8 @@
+function save(){
+	const element = document.getElementById('firstname');
+	console.log(element.value);
+	
+	const elements = document.getElementsByTagName('input');
+	console.log(elements);
+	
+}
